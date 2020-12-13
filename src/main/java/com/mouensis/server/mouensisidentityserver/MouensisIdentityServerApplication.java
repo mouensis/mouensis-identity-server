3146d77cde644c8037afeddf1a0c857f55bb3383
zhuyuan <zhuyuan@unitechs.com>
2020-12-13 21:36:19 +0800

    Add Empty Boot sample

---
+++ src/main/java/com/mouensis/server/mouensisidentityserver/MouensisIdentityServerApplication.java
@@ -0,0 +1,13 @@
+package com.mouensis.server.mouensisidentityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MouensisIdentityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MouensisIdentityServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mouensis/server/mouensisidentityserver/MouensisIdentityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouensis.server.mouensisidentityserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MouensisIdentityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
